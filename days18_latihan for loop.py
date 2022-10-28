60e6372e30aaaaf9a6af586f754f20c564a12987
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-28 05:17:02 -0700

    Add files via upload

---
+++ days18_latihan for loop.py
@@ -0,0 +1,11 @@
+#program yang menerima inputan berupa angka, kemudia angka tersebut di list dari 1 - angka inputan
+angka= int(input("masukkan angka:"))
+
+for i in range (1,angka+):
+    print(i)
+
+
+
+
+    
+    
