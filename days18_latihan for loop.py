e66fa58b865e5889e99a3d4d59f778d73212d6fc
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-18 08:39:32 +0800

    Update days18_latihan for loop.py

--- days18_latihan for loop.py
+++ days18_latihan for loop.py
@@ -1,7 +1,7 @@
 #program yang menerima inputan berupa angka, kemudia angka tersebut di list dari 1 - angka inputan
 angka= int(input("masukkan angka:"))
 
-for i in range (1,angka+):
+for i in range (1,angka+1):
     print(i)
 
 
